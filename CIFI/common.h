581490cc805269e14505ca52b56adb2694ea5fe3
kasunJKD <kasun.ravihara@gmail.com>
2025-02-06 16:55:11 +0530

    cifi bytecode chapter done

---
+++ CIFI/common.h
@@ -0,0 +1,8 @@
+#ifndef cifi_common_h
+#define cifi_common_h
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#endif
---
+++ CIFI/debug.h
@@ -0,0 +1,9 @@
+#ifndef cifi_debug_h
+#define cifi_debug_h
+
+#include "chunk.h"
+
+void disassembleChunk(Chunk* chunk, const char* name);
+int disassembleInstruction(Chunk* chunk, int offset);
+
+#endif
